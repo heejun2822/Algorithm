3dbb899570421ba6ee67fa29e8a09702238ad5e7
heejun2822 <godfam44@gmail.com>
2024-11-08 12:16:00 +0900

    new solution : BOJ_02566

---
+++ BOJ/BOJ_02566/Solution.cs
@@ -0,0 +1,31 @@
+namespace Algorithm.BOJ.BOJ_02566
+{
+    public class Solution
+    {
+        public static string[] InputPaths { get; private set; } =
+        [
+            "BOJ/BOJ_02566/input.txt",
+        ];
+
+        public static void Run(string[] args)
+        {
+            int max = 0;
+            int r = 1;
+            int c = 1;
+            for (int i = 0; i < 9; i++)
+            {
+                string[] row = Console.ReadLine()!.Split();
+                for (int j = 0; j < 9; j++)
+                {
+                    int ele = int.Parse(row[j]);
+                    if (ele <= max) continue;
+                    max = ele;
+                    r = i + 1;
+                    c = j + 1;
+                }
+            }
+            Console.WriteLine(max);
+            Console.WriteLine($"{r} {c}");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using Algorithm.BOJ.BOJ_02738;
+﻿using Algorithm.BOJ.BOJ_02566;
 
 foreach (string inputPath in Solution.InputPaths)
 {
