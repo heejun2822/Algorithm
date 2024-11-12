f6e88ca275ce80dd343aee0accdbb65358cc1060
heejun2822 <godfam44@gmail.com>
2024-11-12 11:39:36 +0900

    new solution : BOJ_02501

---
+++ BOJ/BOJ_02501/Solution.cs
@@ -0,0 +1,32 @@
+namespace Algorithm.BOJ.BOJ_02501
+{
+    public class Solution
+    {
+        public static string[] InputPaths { get; private set; } =
+        [
+            "BOJ/BOJ_02501/input1.txt",
+            "BOJ/BOJ_02501/input2.txt",
+            "BOJ/BOJ_02501/input3.txt",
+        ];
+
+        public static void Run(string[] args)
+        {
+            string[] nk = Console.ReadLine()!.Split();
+            int N = int.Parse(nk[0]);
+            int K = int.Parse(nk[1]);
+            Console.WriteLine(FindFactor(N, K));
+        }
+
+        private static int FindFactor(int N, int K)
+        {
+            if (K == 1) return 1;
+            int cnt = 1;
+            for (int i = 2; i <= N / 2; i++)
+            {
+                if (N % i == 0 && ++cnt == K) return i;
+            }
+            if (cnt == K - 1) return N;
+            return 0;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using Algorithm.BOJ.BOJ_05086;
+﻿using Algorithm.BOJ.BOJ_02501;
 
 foreach (string inputPath in Solution.InputPaths)
 {
