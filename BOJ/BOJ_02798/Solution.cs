6d84b6743e9944ed7c5b864a09ff3e8cb75c62ff
heejun2822 <godfam44@gmail.com>
2024-11-15 17:31:36 +0900

    new solution : BOJ_02798

---
+++ BOJ/BOJ_02798/Solution.cs
@@ -0,0 +1,43 @@
+namespace Algorithm.BOJ.BOJ_02798
+{
+    public class Solution
+    {
+        public static string[] InputPaths { get; private set; } =
+        [
+            "BOJ/BOJ_02798/input1.txt",
+            "BOJ/BOJ_02798/input2.txt",
+        ];
+
+        private static int N;
+        private static int M;
+        private static int[] cards = {};
+
+        public static void Run(string[] args)
+        {
+            string[] nm = Console.ReadLine()!.Split();
+            N = int.Parse(nm[0]);
+            M = int.Parse(nm[1]);
+            cards = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+            Console.WriteLine(ComputeMaxSum());
+        }
+
+        private static int ComputeMaxSum()
+        {
+            int maxSum = 0;
+            for (int i = 0; i < N - 2; i++)
+            {
+                for (int j = i + 1; j < N - 1; j++)
+                {
+                    for (int k = j + 1; k < N; k++)
+                    {
+                        int sum = cards[i] + cards[j] + cards[k];
+                        if (sum > M) continue;
+                        if (sum == M) return M;
+                        if (sum > maxSum) maxSum = sum;
+                    }
+                }
+            }
+            return maxSum;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using Algorithm.BOJ.BOJ_24313;
+﻿using Algorithm.BOJ.BOJ_02798;
 
 foreach (string inputPath in Solution.InputPaths)
 {
