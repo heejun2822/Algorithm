089ff45276e9426b108e0a8c352f064b40c7cbc6
heejun2822 <godfam44@gmail.com>
2024-11-21 17:48:37 +0900

    new solution : BOJ_16395

---
+++ BOJ/BOJ_16395/Solution.cs
@@ -0,0 +1,32 @@
+namespace Algorithm.BOJ.BOJ_16395
+{
+    public class Solution
+    {
+        public static string[] InputPaths { get; private set; } =
+        [
+            "BOJ/BOJ_16395/input1.txt",
+            "BOJ/BOJ_16395/input2.txt",
+        ];
+
+        public static void Run(string[] args)
+        {
+            string[] nk = Console.ReadLine()!.Split();
+            int n = int.Parse(nk[0]);
+            int k = int.Parse(nk[1]);
+            bool[] numbers = new bool[k];
+            int answer = 1;
+            for (int i = n - 1; i > n - k; i--)
+            {
+                answer *= i;
+                for (int j = 2; j < k; j++)
+                {
+                    if (numbers[j]) continue;
+                    if (answer % j != 0) continue;
+                    answer /= j;
+                    numbers[j] = true;
+                }
+            }
+            Console.WriteLine(answer);
+        }
+    }
+}
---
+++ BOJ/BOJ_16395/Solution2.cs
@@ -0,0 +1,32 @@
+namespace Algorithm.BOJ.BOJ_16395
+{
+    public class Solution2
+    {
+        public static string[] InputPaths { get; private set; } =
+        [
+            "BOJ/BOJ_16395/input1.txt",
+            "BOJ/BOJ_16395/input2.txt",
+        ];
+
+        private static int[][] pascals = new int[1][];
+
+        public static void Run(string[] args)
+        {
+            string[] nk = Console.ReadLine()!.Split();
+            int n = int.Parse(nk[0]);
+            int k = int.Parse(nk[1]);
+            pascals = new int[n][];
+            for (int i = 0; i < n; i++) pascals[i] = new int[i + 1];
+            Console.WriteLine(GetPascal(n, k));
+        }
+
+        private static int GetPascal(int n, int k)
+        {
+            if (pascals[n - 1][k - 1] == 0)
+            {
+                pascals[n - 1][k - 1] = (k == 1 || k == n) ? 1 : GetPascal(n - 1, k - 1) + GetPascal(n - 1, k);
+            }
+            return pascals[n - 1][k - 1];
+        }
+    }
+}
