c0885fce4068a263e26c2c090cd06fee9b235acc
heejun2822 <godfam44@gmail.com>
2024-11-26 15:51:18 +0900

    new solution : BOJ_01934

---
+++ BOJ/BOJ_01934/Solution.cs
@@ -0,0 +1,36 @@
+namespace Algorithm.BOJ.BOJ_01934
+{
+    using System.Text;
+
+    public class Solution
+    {
+        public static string[] InputPaths { get; private set; } =
+        [
+            "BOJ/BOJ_01934/input.txt",
+        ];
+
+        public static void Run(string[] args)
+        {
+            int T = int.Parse(Console.ReadLine()!);
+            StringBuilder answer = new();
+            for (int _ = 0; _ < T; _++)
+            {
+                string[] ab = Console.ReadLine()!.Split();
+                int A = int.Parse(ab[0]);
+                int B = int.Parse(ab[1]);
+                // 유클리드 호제법
+                if (A < B) (A, B) = (B, A);
+                int L = A * B;
+                int G = 1;
+                while (B > 0)
+                {
+                    (A, B) = (B, A % B);
+                    G = A;
+                }
+                L /= G;
+                answer.AppendLine(L.ToString());
+            }
+            Console.WriteLine(answer);
+        }
+    }
+}
