b68c93709f370425b1f9bc7b6fe0922c01024577
heejun2822 <godfam44@gmail.com>
2024-11-27 12:26:58 +0900

    new solution : BOJ_25625

---
+++ BOJ/BOJ_25625/Solution.cs
@@ -0,0 +1,19 @@
+namespace Algorithm.BOJ.BOJ_25625
+{
+    public class Solution
+    {
+        public static string[] InputPaths { get; private set; } =
+        [
+            "BOJ/BOJ_25625/input1.txt",
+            "BOJ/BOJ_25625/input2.txt",
+        ];
+
+        public static void Run(string[] args)
+        {
+            string[] inputs = Console.ReadLine()!.Split();
+            int x = int.Parse(inputs[0]);
+            int y = int.Parse(inputs[1]);
+            Console.WriteLine(y < x ? y + x : y - x);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using Algorithm.BOJ.BOJ_18301;
+﻿using Algorithm.BOJ.BOJ_25625;
 
 namespace Algorithm
 {
