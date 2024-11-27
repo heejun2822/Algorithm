2f26d38f4a2ebf287b38024301caf18f86035628
heejun2822 <godfam44@gmail.com>
2024-11-27 11:34:04 +0900

    new solution : BOJ_17103

---
+++ BOJ/BOJ_17103/Solution.cs
@@ -0,0 +1,39 @@
+namespace Algorithm.BOJ.BOJ_17103
+{
+    using System.Text;
+
+    public class Solution
+    {
+        public static string[] InputPaths { get; private set; } =
+        [
+            "BOJ/BOJ_17103/input.txt",
+        ];
+
+        public static void Run(string[] args)
+        {
+            int T = int.Parse(Console.ReadLine()!);
+            int[] inputs = new int[T];
+            for (int i = 0; i < T; i++) inputs[i] = int.Parse(Console.ReadLine()!);
+            int size = inputs.Max();
+            bool[] isPrime = Enumerable.Repeat(true, size + 1).ToArray();
+            isPrime[0] = isPrime[1] = false;
+            int limit = (int)Math.Sqrt(size);
+            for (int num = 2; num <= limit; num++)
+            {
+                if (!isPrime[num]) continue;
+                for (int mul = 2 * num; mul <= size; mul += num) isPrime[mul] = false;
+            }
+            StringBuilder answer = new();
+            foreach (int N in inputs)
+            {
+                int cnt = 0;
+                for (int i = 2; i <= N / 2; i++)
+                {
+                    if (isPrime[i] && isPrime[N - i]) cnt++;
+                }
+                answer.AppendLine(cnt.ToString());
+            }
+            Console.WriteLine(answer);
+        }
+    }
+}
---
+++ BOJ/BOJ_17103/Solution2.cs
@@ -0,0 +1,37 @@
+namespace Algorithm.BOJ.BOJ_17103
+{
+    using System.Text;
+
+    public class Solution2
+    {
+        public static string[] InputPaths { get; private set; } =
+        [
+            "BOJ/BOJ_17103/input.txt",
+        ];
+
+        public static void Run(string[] args)
+        {
+            int size = 1000000;
+            bool[] isPrime = new bool[size + 1];
+            Array.Fill(isPrime, true, 2, isPrime.Length - 2);
+            for (int num = 2; num <= 1000; num++) // Math.Sqrt(size)
+            {
+                if (!isPrime[num]) continue;
+                for (int mul = 2 * num; mul <= size; mul += num) isPrime[mul] = false;
+            }
+            int T = int.Parse(Console.ReadLine()!);
+            StringBuilder answer = new();
+            for (int _ = 0; _ < T; _++)
+            {
+                int N = int.Parse(Console.ReadLine()!);
+                int cnt = 0;
+                for (int i = 2; i <= N / 2; i++)
+                {
+                    if (isPrime[i] && isPrime[N - i]) cnt++;
+                }
+                answer.AppendLine(cnt.ToString());
+            }
+            Console.WriteLine(answer);
+        }
+    }
+}
