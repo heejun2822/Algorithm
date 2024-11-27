310f28d20f6d287a4b7c6a76e27d90e8a441a783
heejun2822 <godfam44@gmail.com>
2024-11-27 14:15:26 +0900

    new solution : BOJ_25870

---
+++ BOJ/BOJ_25870/Solution.cs
@@ -0,0 +1,30 @@
+namespace Algorithm.BOJ.BOJ_25870
+{
+    public class Solution
+    {
+        public static string[] InputPaths { get; private set; } =
+        [
+            "BOJ/BOJ_25870/input1.txt",
+            "BOJ/BOJ_25870/input2.txt",
+            "BOJ/BOJ_25870/input3.txt",
+        ];
+
+        public static void Run(string[] args)
+        {
+            string str = Console.ReadLine()!;
+            int[] counts = new int[26];
+            foreach (char c in str) counts[c - 'a']++;
+            int even = 0, odd = 0;
+            foreach (int cnt in counts)
+            {
+                if (cnt == 0) continue;
+                if (cnt % 2 == 0) even++;
+                else odd++;
+            }
+            int answer = 2;
+            if (even == 0) answer = 1;
+            else if (odd == 0) answer = 0;
+            Console.WriteLine(answer);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using Algorithm.BOJ.BOJ_03765;
+﻿using Algorithm.BOJ.BOJ_25870;
 
 namespace Algorithm
 {
