73b9112171e2a999c3e9cbf6437a9d56ce659d56
heejun2822 <godfam44@gmail.com>
2024-11-28 12:08:39 +0900

    new solution : BOJ_10773

---
+++ BOJ/BOJ_10773/Solution.cs
@@ -0,0 +1,29 @@
+namespace Algorithm.BOJ.BOJ_10773
+{
+    public class Solution
+    {
+        public static string[] InputPaths { get; private set; } =
+        [
+            "BOJ/BOJ_10773/input1.txt",
+            "BOJ/BOJ_10773/input2.txt",
+        ];
+
+        public static void Run(string[] args)
+        {
+            Stack<int> stack = new();
+
+            int K = int.Parse(Console.ReadLine()!);
+            for (int _ = 0; _ < K; _++)
+            {
+                int num = int.Parse(Console.ReadLine()!);
+                if (num == 0) stack.Pop();
+                else stack.Push(num);
+            }
+
+            int sum = 0;
+            foreach (int num in stack) sum += num;
+
+            Console.WriteLine(sum);
+        }
+    }
+}
