4dbe9c65dabc3a4a15524ed6a23b056d445f062f
heejun2822 <godfam44@gmail.com>
2024-12-04 10:25:14 +0900

    new solution : BOJ_27433

---
+++ BOJ/BOJ_27433/Solution.cs
@@ -0,0 +1,23 @@
+namespace Algorithm.BOJ.BOJ_27433
+{
+    public class Solution
+    {
+        public static string[] InputPaths { get; private set; } =
+        [
+            "BOJ/BOJ_27433/input1.txt",
+            "BOJ/BOJ_27433/input2.txt",
+        ];
+
+        public static void Run(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine()!);
+            Console.WriteLine(Factorial(N));
+        }
+
+        private static long Factorial(int n)
+        {
+            if (n == 0) return 1;
+            return n * Factorial(n - 1);
+        }
+    }
+}
