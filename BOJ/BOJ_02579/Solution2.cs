1b3c55e370178bb0e7e9ac15de3a4e2b76031f2d
heejun2822 <godfam44@gmail.com>
2024-12-18 11:32:16 +0900

    add solution : BOJ_02579

---
+++ BOJ/BOJ_02579/Solution2.cs
@@ -0,0 +1,26 @@
+namespace Algorithm.BOJ.BOJ_02579
+{
+    public class Solution2
+    {
+        public static string[] InputPaths { get; private set; } =
+        [
+            "BOJ/BOJ_02579/input.txt",
+        ];
+
+        public static void Run(string[] args)
+        {
+            int cnt = int.Parse(Console.ReadLine()!);
+            int[] stairs = new int[cnt];
+            for (int i = 0; i < cnt; i++) stairs[i] = int.Parse(Console.ReadLine()!);
+
+            int[] acc = new int[cnt];
+            for (int i = 0; i < cnt; i++)
+                acc[i] = Math.Max(
+                    i >= 2 ? acc[i - 2] : 0,
+                    (i >= 3 ? acc[i - 3] : 0) + (i >= 1 ? stairs[i - 1] : 0)
+                ) + stairs[i];
+
+            Console.WriteLine(acc[cnt - 1]);
+        }
+    }
+}
