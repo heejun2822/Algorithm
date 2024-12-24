36e2a91ec1b9a8e78d9588180b5d7c294c3e17ef
heejun2822 <godfam44@gmail.com>
2024-12-24 12:17:57 +0900

    new solution : BOJ_11660

---
+++ BOJ/BOJ_11660/Solution.cs
@@ -0,0 +1,46 @@
+namespace Algorithm.BOJ.BOJ_11660
+{
+    public class Solution
+    {
+        public static string[] InputPaths { get; private set; } =
+        [
+            "BOJ/BOJ_11660/input1.txt",
+            "BOJ/BOJ_11660/input2.txt",
+        ];
+
+        public static void Run(string[] args)
+        {
+            StreamReader sr = Program.InputReader;
+            // StreamReader sr = new(Console.OpenStandardInput());
+            StreamWriter sw = new(Console.OpenStandardOutput());
+
+            string[] nm = sr.ReadLine()!.Split();
+            int N = int.Parse(nm[0]);
+            int M = int.Parse(nm[1]);
+            int[,] arr = new int[N + 1, N + 1];
+            for (int i = 1; i <= N; i++)
+            {
+                string[] numbers = sr.ReadLine()!.Split();
+                for (int j = 1; j <= N; j++)
+                    arr[i, j] = int.Parse(numbers[j - 1]);
+            }
+
+            for (int i = 1; i <= N; i++)
+                for (int j = 1; j <= N; j++)
+                    arr[i, j] += arr[i, j - 1];
+            for (int i = 1; i <= N; i++)
+                for (int j = 1; j <= N; j++)
+                    arr[i, j] += arr[i - 1, j];
+
+            for (int _ = 0; _ < M; _++)
+            {
+                int[] input = Array.ConvertAll(sr.ReadLine()!.Split(), int.Parse);
+                int x1 = input[0], y1 = input[1], x2 = input[2], y2 = input[3];
+                sw.WriteLine(arr[x2, y2] - arr[x2, y1 - 1] - arr[x1 - 1, y2] + arr[x1 - 1, y1 - 1]);
+            }
+
+            sr.Close();
+            sw.Close();
+        }
+    }
+}
