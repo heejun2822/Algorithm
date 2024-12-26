f05fd68c50d7f66e8275c6d78610fa545539002e
heejun2822 <godfam44@gmail.com>
2024-12-26 14:29:52 +0900

    new solution : BOJ_13305

---
+++ BOJ/BOJ_13305/Solution.cs
@@ -0,0 +1,39 @@
+namespace Algorithm.BOJ.BOJ_13305
+{
+    public class Solution
+    {
+        public static string[] InputPaths { get; private set; } =
+        [
+            "BOJ/BOJ_13305/input1.txt",
+            "BOJ/BOJ_13305/input2.txt",
+        ];
+
+        public static void Run(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine()!);
+            int[] roads = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+            (int price, int city)[] gasStations = Console.ReadLine()!.Split().Select((p, i) => (int.Parse(p), i)).ToArray();
+            Array.Sort(gasStations);
+
+            int idx = 0, destination = N - 1;
+            long expense = 0;
+            while (idx < N)
+            {
+                (int price, int city) = gasStations[idx];
+                while (++idx < N)
+                {
+                    if (gasStations[idx].price != price) break;
+                    city = Math.Min(city, gasStations[idx].city);
+                }
+                if (city >= destination) continue;
+
+                long distance = 0;
+                for (int i = city; i < destination; i++) distance += roads[i];
+                expense += price * distance;
+                destination = city;
+            }
+
+            Console.WriteLine(expense);
+        }
+    }
+}
---
+++ BOJ/BOJ_13305/Solution2.cs
@@ -0,0 +1,28 @@
+namespace Algorithm.BOJ.BOJ_13305
+{
+    public class Solution2
+    {
+        public static string[] InputPaths { get; private set; } =
+        [
+            "BOJ/BOJ_13305/input1.txt",
+            "BOJ/BOJ_13305/input2.txt",
+        ];
+
+        public static void Run(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine()!);
+            int[] roads = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+            int[] prices = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+
+            long expense = 0;
+            long minPrice = (long)1e9;
+            for (int i = 0; i < N - 1; i++)
+            {
+                minPrice = Math.Min(minPrice, prices[i]);
+                expense += minPrice * roads[i];
+            }
+
+            Console.WriteLine(expense);
+        }
+    }
+}
