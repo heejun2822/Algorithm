365b37f52133ce7e0bcfb8d948339fe4fd0692b5
heejun2822 <godfam44@gmail.com>
2024-12-26 11:25:57 +0900

    new solution : BOJ_01931

---
+++ BOJ/BOJ_01931/Solution.cs
@@ -0,0 +1,31 @@
+namespace Algorithm.BOJ.BOJ_01931
+{
+    public class Solution
+    {
+        public static string[] InputPaths { get; private set; } =
+        [
+            "BOJ/BOJ_01931/input.txt",
+        ];
+
+        public static void Run(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine()!);
+            (int s, int e)[] meetings = new (int, int)[N];
+            for (int i = 0; i < N; i++)
+            {
+                string[] info = Console.ReadLine()!.Split();
+                meetings[i] = (int.Parse(info[0]), int.Parse(info[1]));
+            }
+            Array.Sort(meetings);
+
+            int t = 0, cnt = 0;
+            foreach ((int s, int e) in meetings)
+            {
+                if (e < t) t = e;
+                else if (s >= t) { t = e; cnt++; }
+            }
+
+            Console.WriteLine(cnt);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿﻿using Algorithm.BOJ.BOJ_11047;
+﻿﻿using Algorithm.BOJ.BOJ_01931;
 
 namespace Algorithm
 {
