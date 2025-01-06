24b8db3cff2937f278df63d8ef22a32ea9e76497
heejun2822 <godfam44@gmail.com>
2025-01-06 11:51:42 +0900

    new solution : BOJ_26596

---
+++ BOJ/BOJ_26596/Solution.cs
@@ -0,0 +1,39 @@
+namespace Algorithm.BOJ.BOJ_26596
+{
+    public class Solution
+    {
+        public static string[] InputPaths { get; private set; } =
+        [
+            "BOJ/BOJ_26596/input1.txt",
+            "BOJ/BOJ_26596/input2.txt",
+        ];
+
+        public static void Run(string[] args)
+        {
+            int M = int.Parse(Console.ReadLine()!);
+            Dictionary<string, int> recipe = new();
+            for (int _ = 0; _ < M; _++)
+            {
+                string[] ingredient = Console.ReadLine()!.Split();
+                string s = ingredient[0];
+                int x = int.Parse(ingredient[1]);
+                if (!recipe.TryAdd(s, x)) recipe[s] += x;
+            }
+
+            foreach ((string si, int xi) in recipe)
+            {
+                int grx = (int)(xi * 1.618f);
+                foreach ((string sj, int xj) in recipe)
+                {
+                    if (sj == si) continue;
+                    if (xj == grx)
+                    {
+                        Console.WriteLine("Delicious!");
+                        return;
+                    }
+                }
+            }
+            Console.WriteLine("Not Delicious...");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿﻿using Algorithm.BOJ.BOJ_12109;
+﻿﻿using Algorithm.BOJ.BOJ_26596;
 
 namespace Algorithm
 {
