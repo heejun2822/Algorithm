666222025d59fa15d716746090685a7345ce06fe
heejun2822 <godfam44@gmail.com>
2025-01-09 19:08:52 +0900

    new solution : BOJ_31428

---
+++ BOJ/BOJ_31428/Solution.cs
@@ -0,0 +1,25 @@
+namespace Algorithm.BOJ.BOJ_31428
+{
+    public class Solution
+    {
+        public static string[] InputPaths { get; private set; } =
+        [
+            "BOJ/BOJ_31428/input1.txt",
+            "BOJ/BOJ_31428/input2.txt",
+        ];
+
+        public static void Run(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine()!);
+            Dictionary<char, int> counts = new() { {'C', 0}, {'S', 0}, {'I', 0}, {'A', 0} };
+            for (int i = 0; i < N; i++)
+            {
+                counts[(char)Console.Read()]++;
+                Console.Read();
+            }
+            char track = char.Parse(Console.ReadLine()!);
+
+            Console.WriteLine(counts[track]);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿﻿using Algorithm.BOJ.BOJ_02110;
+﻿﻿using Algorithm.BOJ.BOJ_31428;
 
 namespace Algorithm
 {
