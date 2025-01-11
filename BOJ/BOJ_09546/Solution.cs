2b3e0ba00a537ddd876114019954abad4ad77e63
Joel <godfam44@gmail.com>
2025-01-11 17:13:22 +0900

    new solution : BOJ_09546

---
+++ BOJ/BOJ_09546/Solution.cs
@@ -0,0 +1,20 @@
+namespace Algorithm.BOJ.BOJ_09546
+{
+    public class Solution
+    {
+        public static string[] InputPaths { get; private set; } =
+        [
+            "BOJ/BOJ_09546/input.txt",
+        ];
+
+        public static void Run(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine()!);
+            for (int _ = 0; _ < t; _++)
+            {
+                int k = int.Parse(Console.ReadLine()!);
+                Console.WriteLine((1 << k) - 1);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿﻿using Algorithm.BOJ.BOJ_12015;
+﻿using Algorithm.BOJ.BOJ_09546;
 
 namespace Algorithm
 {
