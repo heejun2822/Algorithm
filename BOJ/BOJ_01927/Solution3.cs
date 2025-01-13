f8c8b8d6796601eaef7605b0dc5020ad9df9207c
heejun2822 <godfam44@gmail.com>
2025-01-13 14:29:37 +0900

    add solution : BOJ_01927

---
+++ BOJ/BOJ_01927/Solution3.cs
@@ -0,0 +1,86 @@
+namespace Algorithm.BOJ.BOJ_01927
+{
+    public class Solution3
+    {
+        public static string[] InputPaths { get; private set; } =
+        [
+            "BOJ/BOJ_01927/input.txt",
+        ];
+
+        public static void Run(string[] args)
+        {
+            StreamReader sr = Program.InputReader;
+            // StreamReader sr = new(Console.OpenStandardInput());
+            StreamWriter sw = new(Console.OpenStandardOutput());
+
+            int N = int.Parse(sr.ReadLine()!);
+            MinHeap minHeap = new(100000);
+            for (int _ = 0; _ < N; _++)
+            {
+                int x = int.Parse(sr.ReadLine()!);
+                if (x == 0) sw.WriteLine(minHeap.TryPop(out int ele) ? ele : "0");
+                else minHeap.Push(x);
+            }
+
+            sr.Close();
+            sw.Close();
+        }
+
+        // 최소 힙 (Min Heap)
+        private class MinHeap
+        {
+            int[] m_Heap;
+            int m_Count;
+
+            public MinHeap(int capacity)
+            {
+                m_Heap = new int[capacity + 1];
+                m_Count = 0;
+            }
+
+            public void Push(int ele)
+            {
+                m_Heap[++m_Count] = ele;
+
+                int cIdx = m_Count;
+                while (cIdx > 1)
+                {
+                    int pIdx = cIdx / 2;
+                    if (m_Heap[cIdx] >= m_Heap[pIdx]) break;
+                    (m_Heap[cIdx], m_Heap[pIdx]) = (m_Heap[pIdx], m_Heap[cIdx]);
+                    cIdx = pIdx;
+                }
+            }
+
+            public int Pop()
+            {
+                int ele = m_Heap[1];
+                m_Heap[1] = m_Heap[m_Count--];
+
+                int pIdx = 1;
+                while (pIdx * 2 <= m_Count)
+                {
+                    int cIdx = pIdx * 2;
+                    if (cIdx + 1 <= m_Count && m_Heap[cIdx + 1] < m_Heap[cIdx]) cIdx++;
+                    if (m_Heap[pIdx] <= m_Heap[cIdx]) break;
+                    (m_Heap[pIdx], m_Heap[cIdx]) = (m_Heap[cIdx], m_Heap[pIdx]);
+                    pIdx = cIdx;
+                }
+
+                return ele;
+            }
+
+            public bool TryPop(out int ele)
+            {
+                if (m_Count == 0)
+                {
+                    ele = 0;
+                    return false;
+                }
+
+                ele = Pop();
+                return true;
+            }
+        }
+    }
+}
