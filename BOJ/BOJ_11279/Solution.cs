e3d53f0926b826171f4ff306064a49eab742af30
heejun2822 <godfam44@gmail.com>
2025-01-13 12:24:36 +0900

    new solution : BOJ_11279

---
+++ BOJ/BOJ_11279/Solution.cs
@@ -0,0 +1,29 @@
+namespace Algorithm.BOJ.BOJ_11279
+{
+    public class Solution
+    {
+        public static string[] InputPaths { get; private set; } =
+        [
+            "BOJ/BOJ_11279/input.txt",
+        ];
+
+        public static void Run(string[] args)
+        {
+            StreamReader sr = Program.InputReader;
+            // StreamReader sr = new(Console.OpenStandardInput());
+            StreamWriter sw = new(Console.OpenStandardOutput());
+
+            int N = int.Parse(sr.ReadLine()!);
+            PriorityQueue<int, int> priorityQueue = new(Comparer<int>.Create((a, b) => b - a));
+            for (int _ = 0; _ < N; _++)
+            {
+                int x = int.Parse(sr.ReadLine()!);
+                if (x == 0) sw.WriteLine(priorityQueue.TryDequeue(out int ele, out int p) ? ele : "0");
+                else priorityQueue.Enqueue(x, x);
+            }
+
+            sr.Close();
+            sw.Close();
+        }
+    }
+}
---
+++ BOJ/BOJ_11279/Solution2.cs
@@ -0,0 +1,81 @@
+namespace Algorithm.BOJ.BOJ_11279
+{
+    public class Solution2
+    {
+        public static string[] InputPaths { get; private set; } =
+        [
+            "BOJ/BOJ_11279/input.txt",
+        ];
+
+        public static void Run(string[] args)
+        {
+            StreamReader sr = Program.InputReader;
+            // StreamReader sr = new(Console.OpenStandardInput());
+            StreamWriter sw = new(Console.OpenStandardOutput());
+
+            int N = int.Parse(sr.ReadLine()!);
+            MaxHeap maxHeap = new();
+            for (int _ = 0; _ < N; _++)
+            {
+                int x = int.Parse(sr.ReadLine()!);
+                if (x == 0) sw.WriteLine(maxHeap.TryPop(out int ele) ? ele : "0");
+                else maxHeap.Push(x);
+            }
+
+            sr.Close();
+            sw.Close();
+        }
+
+        private class MaxHeap
+        {
+            List<int> m_List = new() {0};
+
+            public int Count { get => m_List.Count - 1; }
+
+            public void Push(int ele)
+            {
+                m_List.Add(ele);
+
+                int cIdx = Count;
+                while (cIdx > 1)
+                {
+                    int pIdx = cIdx / 2;
+                    if (m_List[cIdx] <= m_List[pIdx]) break;
+                    (m_List[cIdx], m_List[pIdx]) = (m_List[pIdx], m_List[cIdx]);
+                    cIdx = pIdx;
+                }
+            }
+
+            public int Pop()
+            {
+                int ele = m_List[1];
+                m_List[1] = m_List[Count];
+                m_List.RemoveAt(Count);
+
+                int pIdx = 1;
+                while (pIdx * 2 <= Count)
+                {
+                    int cIdx = pIdx * 2;
+                    if (cIdx + 1 <= Count && m_List[cIdx + 1] > m_List[cIdx]) cIdx++;
+                    if (m_List[pIdx] > m_List[cIdx]) break;
+                    (m_List[pIdx], m_List[cIdx]) = (m_List[cIdx], m_List[pIdx]);
+                    pIdx = cIdx;
+                }
+
+                return ele;
+            }
+
+            public bool TryPop(out int ele)
+            {
+                if (Count == 0)
+                {
+                    ele = 0;
+                    return false;
+                }
+
+                ele = Pop();
+                return true;
+            }
+        }
+    }
+}
