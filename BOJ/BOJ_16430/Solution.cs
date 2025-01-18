0f43ba1bdbb2f50a5feb41a6023f978eab31466f
Joel <godfam44@gmail.com>
2025-01-18 18:31:00 +0900

    new solution : BOJ_16430

---
+++ BOJ/BOJ_16430/Solution.cs
@@ -0,0 +1,20 @@
+namespace Algorithm.BOJ.BOJ_16430
+{
+    public class Solution
+    {
+        public static string[] InputPaths { get; private set; } =
+        [
+            "BOJ/BOJ_16430/input1.txt",
+            "BOJ/BOJ_16430/input2.txt",
+        ];
+
+        public static void Run(string[] args)
+        {
+            string[] ab = Console.ReadLine()!.Split();
+            int A = int.Parse(ab[0]);
+            int B = int.Parse(ab[1]);
+
+            Console.WriteLine($"{B - A} {B}");
+        }
+    }
+}
