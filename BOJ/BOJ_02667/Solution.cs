acceb016b046774c417cc04c719a4f56928e6217
heejun2822 <godfam44@gmail.com>
2025-02-03 11:52:00 +0900

    new solution : BOJ_02667

---
+++ BOJ/BOJ_02667/Solution.cs
@@ -0,0 +1,57 @@
+namespace Algorithm.BOJ.BOJ_02667
+{
+    public class Solution
+    {
+        public static string[] InputPaths { get; private set; } =
+        [
+            "BOJ/BOJ_02667/input.txt",
+        ];
+
+        private static readonly int[] dr = { 0, 0, 1, -1 };
+        private static readonly int[] dc = { 1, -1, 0, 0 };
+
+        private static int N;
+        private static bool[,] map = {};
+
+        public static void Run(string[] args)
+        {
+            N = int.Parse(Console.ReadLine()!);
+            map = new bool[N, N];
+            for (int i = 0; i < N; i++)
+            {
+                string line = Console.ReadLine()!;
+                for (int j = 0; j < N; j++) map[i, j] = line[j] == '1';
+            }
+
+            List<int> complexes = new();
+
+            for (int r = 0; r < N; r++)
+            {
+                for (int c = 0; c < N; c++)
+                {
+                    if (!map[r, c]) continue;
+                    complexes.Add(CountHouses(r, c));
+                }
+            }
+
+            complexes.Sort();
+
+            Console.WriteLine(complexes.Count);
+            foreach (int cnt in complexes) Console.WriteLine(cnt);
+        }
+
+        private static int CountHouses(int r, int c)
+        {
+            if (r == -1 || r == N || c == -1 || c == N) return 0;
+            if (!map[r, c]) return 0;
+
+            map[r, c] = false;
+            int cnt = 1;
+
+            for (int i = 0; i < 4; i++)
+                cnt += CountHouses(r + dr[i], c + dc[i]);
+
+            return cnt;
+        }
+    }
+}
---
+++ BOJ/BOJ_02667/Solution2.cs
@@ -0,0 +1,55 @@
+namespace Algorithm.BOJ.BOJ_02667
+{
+    public class Solution2
+    {
+        public static string[] InputPaths { get; private set; } =
+        [
+            "BOJ/BOJ_02667/input.txt",
+        ];
+
+        private static int N;
+        private static bool[,] map = {};
+
+        public static void Run(string[] args)
+        {
+            N = int.Parse(Console.ReadLine()!);
+            map = new bool[N, N];
+            for (int i = 0; i < N; i++)
+            {
+                string line = Console.ReadLine()!;
+                for (int j = 0; j < N; j++) map[i, j] = line[j] == '1';
+            }
+
+            List<int> complexes = new();
+
+            for (int r = 0; r < N; r++)
+            {
+                for (int c = 0; c < N; c++)
+                {
+                    if (!map[r, c]) continue;
+                    complexes.Add(CountHouses(r, c));
+                }
+            }
+
+            complexes.Sort();
+
+            Console.WriteLine(complexes.Count);
+            foreach (int cnt in complexes) Console.WriteLine(cnt);
+        }
+
+        private static int CountHouses(int r, int c)
+        {
+            if (!map[r, c]) return 0;
+
+            map[r, c] = false;
+
+            int cnt = 1;
+            if (r > 0) cnt += CountHouses(r - 1, c);
+            if (r < N - 1) cnt += CountHouses(r + 1, c);
+            if (c > 0) cnt += CountHouses(r, c - 1);
+            if (c < N - 1) cnt += CountHouses(r, c + 1);
+
+            return cnt;
+        }
+    }
+}
