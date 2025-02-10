7b8dfcab2ce93fa3b56b4a5131c91aec11c28af0
heejun2822 <godfam44@gmail.com>
2025-02-10 11:57:26 +0900

    new solution : BOJ_07656

---
+++ BOJ/BOJ_07656/Solution.cs
@@ -0,0 +1,45 @@
+namespace Algorithm.BOJ.BOJ_07656
+{
+    public class Solution
+    {
+        public static string[] InputPaths { get; private set; } =
+        [
+            "BOJ/BOJ_07656/input.txt",
+        ];
+
+        public static void Run(string[] args)
+        {
+            string input = Console.ReadLine()!;
+            string q = "What is";
+
+            int idx = 0;
+            while (idx + q.Length - 1 < input.Length)
+            {
+                int s = idx + q.Length;
+                for (int i = 0; i < q.Length; i++)
+                {
+                    if (input[idx++] != q[i])
+                    {
+                        s = -1;
+                        break;
+                    }
+                }
+
+                if (s == -1) continue;
+
+                idx--;
+                while (++idx < input.Length)
+                {
+                    if (input[idx] == '.') break;
+                    if (input[idx] == '?')
+                    {
+                        Console.Write("Forty-two is");
+                        for (int i = s; i < idx; i++) Console.Write(input[i]);
+                        Console.WriteLine(".");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using Algorithm.BOJ.BOJ_24855;
+﻿﻿using Algorithm.BOJ.BOJ_07656;
 
 namespace Algorithm
 {
