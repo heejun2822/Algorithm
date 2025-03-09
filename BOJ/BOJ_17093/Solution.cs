028f49c233d15207c6e09f24c5f25ef0f3203001
Joel <godfam44@gmail.com>
2025-03-09 23:44:37 +0900

    new solution : BOJ_17093

---
+++ BOJ/BOJ_17093/Solution.cs
@@ -0,0 +1,46 @@
+namespace Algorithm.BOJ.BOJ_17093
+{
+    public class Solution
+    {
+        public static string[] InputPaths { get; private set; } =
+        [
+            "BOJ/BOJ_17093/input1.txt",
+            "BOJ/BOJ_17093/input2.txt",
+        ];
+
+        public static void Run(string[] args)
+        {
+            StreamReader sr = Program.InputReader;
+            // StreamReader sr = new(new BufferedStream(Console.OpenStandardInput()));
+            StreamWriter sw = new(new BufferedStream(Console.OpenStandardOutput()));
+
+            string[] nm = sr.ReadLine()!.Split();
+            int N = int.Parse(nm[0]);
+            int M = int.Parse(nm[1]);
+
+            (int x, int y)[] P = new (int, int)[N];
+            for (int i = 0; i < N; i++)
+            {
+                string[] xy = sr.ReadLine()!.Split();
+                P[i] = (int.Parse(xy[0]), int.Parse(xy[1]));
+            }
+
+            long maxRS = 0;
+
+            for (int i = 0; i < M; i++)
+            {
+                string[] xy = sr.ReadLine()!.Split();
+                long Qix = long.Parse(xy[0]);
+                long Qiy = long.Parse(xy[1]);
+
+                for (int j = 0; j < N; j++)
+                    maxRS = Math.Max(maxRS, (Qix - P[j].x) * (Qix - P[j].x) + (Qiy - P[j].y) * (Qiy - P[j].y));
+            }
+
+            sw.WriteLine(maxRS);
+
+            sr.Close();
+            sw.Close();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using Algorithm.BOJ.BOJ_09469;
+﻿using Algorithm.BOJ.BOJ_17093;
 
 namespace Algorithm
 {
