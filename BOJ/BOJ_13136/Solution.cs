8b9ce447c185863f8ba6a3bb496f1d1960edf489
Joel <godfam44@gmail.com>
2025-03-22 17:39:13 +0900

    new solution : BOJ_13136

---
+++ BOJ/BOJ_13136/Solution.cs
@@ -0,0 +1,20 @@
+namespace Algorithm.BOJ.BOJ_13136
+{
+    public class Solution
+    {
+        public static string[] InputPaths { get; private set; } =
+        [
+            "BOJ/BOJ_13136/input.txt",
+        ];
+
+        public static void Run(string[] args)
+        {
+            string[] rcn = Console.ReadLine()!.Split();
+            int R = int.Parse(rcn[0]);
+            int C = int.Parse(rcn[1]);
+            int N = int.Parse(rcn[2]);
+
+            Console.WriteLine((long)((R + N - 1) / N) * ((C + N - 1) / N));
+        }
+    }
+}
