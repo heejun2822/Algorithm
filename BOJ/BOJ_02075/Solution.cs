b1e2e13e4f32cdf6e5cbe86571b21bdeae5a8ff6
Joel <godfam44@gmail.com>
2025-04-16 13:44:34 +0900

    new solution : BOJ_02075

---
+++ BOJ/BOJ_02075/Solution.cs
@@ -0,0 +1,60 @@
+namespace Algorithm.BOJ.BOJ_02075
+{
+    public class Solution
+    {
+        public static string[] InputPaths { get; private set; } =
+        [
+            "BOJ/BOJ_02075/input.txt",
+        ];
+
+        public static void Run(string[] args)
+        {
+            StreamReader sr = Program.InputReader;
+            // StreamReader sr = new(new BufferedStream(Console.OpenStandardInput()));
+            StreamWriter sw = new(new BufferedStream(Console.OpenStandardOutput()));
+
+            int N = ReadInt(sr);
+
+            Stack<int>[] stacks = new Stack<int>[N];
+            for (int i = 0; i < N; i++) stacks[i] = new(N - 1);
+
+            for (int _ = 0; _ < N - 1; _++)
+                for (int i = 0; i < N; i++)
+                    stacks[i].Push(ReadInt(sr));
+
+            PriorityQueue<(int val, int idx), int> priorityQueue = new(N);
+
+            for (int i = 0; i < N; i++)
+            {
+                int num = ReadInt(sr);
+                priorityQueue.Enqueue((num, i), -num);
+            }
+
+            for (int _ = 0; _ < N - 1; _++)
+            {
+                int idx = priorityQueue.Dequeue().idx;
+                int num = stacks[idx].Pop();
+                priorityQueue.Enqueue((num, idx), -num);
+            }
+
+            sw.WriteLine(priorityQueue.Dequeue().val);
+
+            sr.Close();
+            sw.Close();
+        }
+
+        private static int ReadInt(StreamReader sr)
+        {
+            int c, val = 0, sign = 1;
+            while (true)
+            {
+                c = sr.Read();
+                if (c == ' ' || c == '\n' || c == -1) break;
+                if (c == '\r') { sr.Read(); break; }
+                if (c == '-') { sign = -1; continue; }
+                val = val * 10 + c - '0';
+            }
+            return val * sign;
+        }
+    }
+}
---
+++ BOJ/BOJ_02075/Solution2.cs
@@ -0,0 +1,57 @@
+namespace Algorithm.BOJ.BOJ_02075
+{
+    public class Solution2
+    {
+        public static string[] InputPaths { get; private set; } =
+        [
+            "BOJ/BOJ_02075/input.txt",
+        ];
+
+        public static void Run(string[] args)
+        {
+            StreamReader sr = Program.InputReader;
+            // StreamReader sr = new(new BufferedStream(Console.OpenStandardInput()));
+            StreamWriter sw = new(new BufferedStream(Console.OpenStandardOutput()));
+
+            int N = ReadInt(sr);
+
+            PriorityQueue<int, int> priorityQueue = new(N);
+
+            for (int _ = 0; _ < N; _++)
+            {
+                int num = ReadInt(sr);
+                priorityQueue.Enqueue(num, num);
+            }
+
+            int cnt = N * (N - 1);
+            for (int _ = 0; _ < cnt; _++)
+            {
+                int num = ReadInt(sr);
+                if (num > priorityQueue.Peek())
+                {
+                    priorityQueue.Dequeue();
+                    priorityQueue.Enqueue(num, num);
+                }
+            }
+
+            sw.WriteLine(priorityQueue.Peek());
+
+            sr.Close();
+            sw.Close();
+        }
+
+        private static int ReadInt(StreamReader sr)
+        {
+            int c, val = 0, sign = 1;
+            while (true)
+            {
+                c = sr.Read();
+                if (c == ' ' || c == '\n' || c == -1) break;
+                if (c == '\r') { sr.Read(); break; }
+                if (c == '-') { sign = -1; continue; }
+                val = val * 10 + c - '0';
+            }
+            return val * sign;
+        }
+    }
+}
