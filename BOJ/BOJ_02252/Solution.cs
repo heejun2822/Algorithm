361f8c89bc429b1b4a62f3d13099af43d79d98fd
Joel <godfam44@gmail.com>
2025-04-23 15:28:00 +0900

    new solution : BOJ_02252

---
+++ BOJ/BOJ_02252/Solution.cs
@@ -0,0 +1,75 @@
+namespace Algorithm.BOJ.BOJ_02252
+{
+    using System.Text;
+
+    public class Solution
+    {
+        public static string[] InputPaths { get; private set; } =
+        [
+            "BOJ/BOJ_02252/input1.txt",
+            "BOJ/BOJ_02252/input2.txt",
+        ];
+
+        public static void Run(string[] args)
+        {
+            StreamReader sr = Program.InputReader;
+            // StreamReader sr = new(new BufferedStream(Console.OpenStandardInput()));
+            StreamWriter sw = new(new BufferedStream(Console.OpenStandardOutput()));
+
+            int N = ReadInt(sr);
+            int M = ReadInt(sr);
+
+            List<int>[] edgeLists = new List<int>[N + 1];
+            int[] inDegrees = new int[N + 1];
+
+            for (int i = 1; i <= N; i++) edgeLists[i] = new();
+            for (int _ = 0; _ < M; _++)
+            {
+                int f = ReadInt(sr);
+                int t = ReadInt(sr);
+                edgeLists[f].Add(t);
+                inDegrees[t]++;
+            }
+
+            Queue<int> queue = new();
+
+            for (int node = 1; node <= N; node++)
+            {
+                if (inDegrees[node] == 0) queue.Enqueue(node);
+            }
+
+            StringBuilder sequence = new();
+
+            while (queue.Count > 0)
+            {
+                int node = queue.Dequeue();
+
+                sequence.Append(node).Append(' ');
+
+                foreach (int nextNode in edgeLists[node])
+                {
+                    if (--inDegrees[nextNode] == 0) queue.Enqueue(nextNode);
+                }
+            }
+
+            sw.WriteLine(sequence);
+
+            sr.Close();
+            sw.Close();
+        }
+
+        private static int ReadInt(StreamReader sr)
+        {
+            int c, val = 0, sign = 1;
+            while (true)
+            {
+                c = sr.Read();
+                if (c == ' ' || c == '\n' || c == -1) break;
+                if (c == '\r') { sr.Read(); break; }
+                if (c == '-') { sign = -1; continue; }
+                val = val * 10 + c - '0';
+            }
+            return val * sign;
+        }
+    }
+}
---
+++ BOJ/BOJ_02252/Solution2.cs
@@ -0,0 +1,67 @@
+namespace Algorithm.BOJ.BOJ_02252
+{
+    using System.Text;
+
+    public class Solution2
+    {
+        public static string[] InputPaths { get; private set; } =
+        [
+            "BOJ/BOJ_02252/input1.txt",
+            "BOJ/BOJ_02252/input2.txt",
+        ];
+
+        public static void Run(string[] args)
+        {
+            StreamReader sr = Program.InputReader;
+            // StreamReader sr = new(new BufferedStream(Console.OpenStandardInput()));
+            StreamWriter sw = new(new BufferedStream(Console.OpenStandardOutput()));
+
+            int N = ReadInt(sr);
+            int M = ReadInt(sr);
+
+            List<int>[] edgeLists = new List<int>[N + 1];
+
+            for (int i = 1; i <= N; i++) edgeLists[i] = new();
+            for (int _ = 0; _ < M; _++) edgeLists[ReadInt(sr)].Add(ReadInt(sr));
+
+            bool[] visited = new bool[N + 1];
+            Stack<int> stack = new();
+
+            for (int node = 1; node <= N; node++) DFS(node);
+
+            StringBuilder sequence = new();
+
+            while (stack.Count > 0) sequence.Append(stack.Pop()).Append(' ');
+
+            sw.WriteLine(sequence);
+
+            sr.Close();
+            sw.Close();
+
+            void DFS(int node)
+            {
+                if (visited[node]) return;
+
+                visited[node] = true;
+
+                foreach (int nextNode in edgeLists[node]) DFS(nextNode);
+
+                stack.Push(node);
+            }
+        }
+
+        private static int ReadInt(StreamReader sr)
+        {
+            int c, val = 0, sign = 1;
+            while (true)
+            {
+                c = sr.Read();
+                if (c == ' ' || c == '\n' || c == -1) break;
+                if (c == '\r') { sr.Read(); break; }
+                if (c == '-') { sign = -1; continue; }
+                val = val * 10 + c - '0';
+            }
+            return val * sign;
+        }
+    }
+}
