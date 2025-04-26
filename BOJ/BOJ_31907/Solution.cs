c58d592b7108dd312c8ba6e2b6b8c3e683507071
Joel <godfam44@gmail.com>
2025-04-26 17:11:10 +0900

    new solution : BOJ_31907

---
+++ BOJ/BOJ_31907/Solution.cs
@@ -0,0 +1,42 @@
+namespace Algorithm.BOJ.BOJ_31907
+{
+    public class Solution
+    {
+        public static string[] InputPaths { get; private set; } =
+        [
+            "BOJ/BOJ_31907/input1.txt",
+            "BOJ/BOJ_31907/input2.txt",
+        ];
+
+        public static void Run(string[] args)
+        {
+            int K = int.Parse(Console.ReadLine()!);
+
+            System.Text.StringBuilder banner = new();
+
+            for (int i = 0; i < K; i++)
+            {
+                for (int j = 0; j < K; j++) banner.Append('G');
+                for (int j = 0; j < K; j++) banner.Append("...");
+                banner.AppendLine();
+            }
+            for (int i = 0; i < K; i++)
+            {
+                for (int j = 0; j < K; j++) banner.Append('.');
+                for (int j = 0; j < K; j++) banner.Append('I');
+                for (int j = 0; j < K; j++) banner.Append('.');
+                for (int j = 0; j < K; j++) banner.Append('T');
+                banner.AppendLine();
+            }
+            for (int i = 0; i < K; i++)
+            {
+                for (int j = 0; j < K; j++) banner.Append("..");
+                for (int j = 0; j < K; j++) banner.Append('S');
+                for (int j = 0; j < K; j++) banner.Append('.');
+                banner.AppendLine();
+            }
+
+            Console.Write(banner);
+        }
+    }
+}
---
+++ BOJ/BOJ_31907/Solution2.cs
@@ -0,0 +1,27 @@
+namespace Algorithm.BOJ.BOJ_31907
+{
+    public class Solution2
+    {
+        public static string[] InputPaths { get; private set; } =
+        [
+            "BOJ/BOJ_31907/input1.txt",
+            "BOJ/BOJ_31907/input2.txt",
+        ];
+
+        public static void Run(string[] args)
+        {
+            int K = int.Parse(Console.ReadLine()!);
+
+            System.Text.StringBuilder banner = new();
+
+            for (int i = 0; i < K; i++)
+                banner.Append(new string('G', K)).Append(new string('.', K * 3)).AppendLine();
+            for (int i = 0; i < K; i++)
+                banner.Append(new string('.', K)).Append(new string('I', K)).Append(new string('.', K)).Append(new string('T', K)).AppendLine();
+            for (int i = 0; i < K; i++)
+                banner.Append(new string('.', K * 2)).Append(new string('S', K)).Append(new string('.', K)).AppendLine();
+
+            Console.Write(banner);
+        }
+    }
+}
