1950e9893a021e7c41ebb8643cf74a246e42925d
Joel <godfam44@gmail.com>
2025-04-29 19:04:05 +0900

    new solution : BOJ_01003

---
+++ BOJ/BOJ_01003/Solution.cs
@@ -0,0 +1,28 @@
+namespace Algorithm.BOJ.BOJ_01003
+{
+    public class Solution
+    {
+        public static string[] InputPaths { get; private set; } =
+        [
+            "BOJ/BOJ_01003/input1.txt",
+            "BOJ/BOJ_01003/input2.txt",
+        ];
+
+        public static void Run(string[] args)
+        {
+            int T = int.Parse(Console.ReadLine()!);
+
+            List<(int zero, int one)> counts = new(41) { (1, 0), (0, 1) };
+
+            for (int _ = 0; _ < T; _++)
+            {
+                int N = int.Parse(Console.ReadLine()!);
+
+                while (counts.Count <= N)
+                    counts.Add((counts[^1].zero + counts[^2].zero, counts[^1].one + counts[^2].one));
+
+                Console.WriteLine($"{counts[N].zero} {counts[N].one}");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using Algorithm.BOJ.BOJ_13567;
+﻿using Algorithm.BOJ.BOJ_01003;
 
 namespace Algorithm
 {
