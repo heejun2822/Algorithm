f4493c2cd79738d12f92b68458173f6c00766f0d
Joel <godfam44@gmail.com>
2025-05-07 15:52:04 +0900

    new solution : BOJ_02748

---
+++ BOJ/BOJ_02748/Solution.cs
@@ -0,0 +1,23 @@
+namespace Algorithm.BOJ.BOJ_02748
+{
+    public class Solution
+    {
+        public static string[] InputPaths { get; private set; } =
+        [
+            "BOJ/BOJ_02748/input.txt",
+        ];
+
+        public static void Run(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine()!);
+
+            long[] F = new long[n + 1];
+            F[0] = 0;
+            F[1] = 1;
+
+            for (int i = 2; i <= n; i++) F[i] = F[i - 2] + F[i - 1];
+
+            Console.WriteLine(F[n]);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using Algorithm.BOJ.BOJ_32801;
+﻿using Algorithm.BOJ.BOJ_02748;
 
 namespace Algorithm
 {
