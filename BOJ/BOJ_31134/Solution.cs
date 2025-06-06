6d37a16ec779e41404823cd3b9e2b9b79fbfa457
heejun2822 <godfam44@gmail.com>
2025-06-06 23:41:51 +0900

    new solution : BOJ_31134

---
+++ BOJ/BOJ_31134/Solution.cs
@@ -0,0 +1,33 @@
+namespace Algorithm.BOJ.BOJ_31134
+{
+    public class Solution
+    {
+        public static string[] InputPaths { get; private set; } =
+        [
+            "BOJ/BOJ_31134/input.txt",
+        ];
+
+        public static void Run(string[] args)
+        {
+            StreamReader sr = Program.InputReader;
+            // StreamReader sr = new(new BufferedStream(Console.OpenStandardInput()));
+            StreamWriter sw = new(new BufferedStream(Console.OpenStandardOutput()));
+
+            int T = int.Parse(sr.ReadLine()!);
+
+            System.Text.StringBuilder answer = new();
+
+            for (int _ = 0; _ < T; _++)
+            {
+                int x = int.Parse(sr.ReadLine()!);
+                long n = 2L * x - 1;
+                answer.AppendLine(n.ToString());
+            }
+
+            sw.Write(answer);
+
+            sr.Close();
+            sw.Close();
+        }
+    }
+}
