91c84cae1a91acccb0edc25351f5a18df3d99cad
heejun2822 <godfam44@gmail.com>
2025-07-23 17:27:59 +0900

    new solution : BOJ_09933

---
+++ BOJ/BOJ_09933/Solution.cs
@@ -0,0 +1,44 @@
+namespace Algorithm.BOJ.BOJ_09933
+{
+    public class Solution
+    {
+        public static string[] InputPaths { get; private set; } =
+        [
+            "BOJ/BOJ_09933/input1.txt",
+            "BOJ/BOJ_09933/input2.txt",
+        ];
+
+        public static void Run(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine()!);
+            string[] words = new string[N];
+
+            for (int i = 0; i < N; i++)
+            {
+                words[i] = Console.ReadLine()!;
+
+                for (int j = 0; j <= i; j++)
+                {
+                    if (IsBackward(words[i], words[j]))
+                    {
+                        int len = words[i].Length;
+                        Console.WriteLine($"{len} {words[i][len / 2]}");
+                        return;
+                    }
+                }
+            }
+
+            bool IsBackward(string A, string B)
+            {
+                if (A.Length != B.Length)
+                    return false;
+
+                for (int i = 0; i < A.Length; i++)
+                    if (A[i] != B[^(i + 1)])
+                        return false;
+
+                return true;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using Algorithm.BOJ.BOJ_11312;
+﻿using Algorithm.BOJ.BOJ_09933;
 
 namespace Algorithm
 {
