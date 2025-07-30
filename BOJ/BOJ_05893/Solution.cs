0ebf3253e67e45d5278b384332781e0877ef5a5f
heejun2822 <godfam44@gmail.com>
2025-07-30 16:40:30 +0900

    new solution : BOJ_05893

---
+++ BOJ/BOJ_05893/Solution.cs
@@ -0,0 +1,42 @@
+namespace Algorithm.BOJ.BOJ_05893
+{
+    public class Solution
+    {
+        public static string[] InputPaths { get; private set; } =
+        [
+            "BOJ/BOJ_05893/input.txt",
+        ];
+
+        public static void Run(string[] args)
+        {
+            string N = Console.ReadLine()!;
+
+            int[] digits = new int[N.Length + 5];
+
+            for (int i = 0; i < N.Length; i++)
+            {
+                int d = N[i] - '0';
+                digits[i + 1] += d;
+                digits[i + 5] += d;
+            }
+
+            for (int i = N.Length + 4; i > 0; i--)
+            {
+                if (digits[i] >= 2)
+                {
+                    digits[i] -= 2;
+                    digits[i - 1]++;
+                }
+            }
+
+            System.Text.StringBuilder output = new();
+
+            if (digits[0] == 1)
+                output.Append(digits[0]);
+            for (int i = 1; i < N.Length + 5; i++)
+                output.Append(digits[i]);
+
+            Console.WriteLine(output);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using Algorithm.BOJ.BOJ_32931;
+﻿using Algorithm.BOJ.BOJ_05893;
 
 namespace Algorithm
 {
