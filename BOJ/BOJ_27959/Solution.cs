320017a79924f7d9dbbcdcfa2f1bfa55c4adaa9d
heejun2822 <godfam44@gmail.com>
2025-08-06 19:10:26 +0900

    new solution : BOJ_27959

---
+++ BOJ/BOJ_27959/Solution.cs
@@ -0,0 +1,21 @@
+namespace Algorithm.BOJ.BOJ_27959
+{
+    public class Solution
+    {
+        public static string[] InputPaths { get; private set; } =
+        [
+            "BOJ/BOJ_27959/input1.txt",
+            "BOJ/BOJ_27959/input2.txt",
+            "BOJ/BOJ_27959/input3.txt",
+        ];
+
+        public static void Run(string[] args)
+        {
+            string[] input = Console.ReadLine()!.Split();
+            int N = int.Parse(input[0]);
+            int M = int.Parse(input[1]);
+
+            Console.WriteLine(100 * N >= M ? "Yes" : "No");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using Algorithm.BOJ.BOJ_20153;
+﻿using Algorithm.BOJ.BOJ_27959;
 
 namespace Algorithm
 {
