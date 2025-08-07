f9842262b908b5f205f842be66cbd8edd988a568
heejun2822 <godfam44@gmail.com>
2025-08-07 13:05:06 +0900

    new solution : BOJ_11816

---
+++ BOJ/BOJ_11816/Solution.cs
@@ -0,0 +1,29 @@
+namespace Algorithm.BOJ.BOJ_11816
+{
+    public class Solution
+    {
+        public static string[] InputPaths { get; private set; } =
+        [
+            "BOJ/BOJ_11816/input1.txt",
+            "BOJ/BOJ_11816/input2.txt",
+            "BOJ/BOJ_11816/input3.txt",
+            "BOJ/BOJ_11816/input4.txt",
+        ];
+
+        public static void Run(string[] args)
+        {
+            string X = Console.ReadLine()!;
+            int decimalX;
+
+            if (X[0] == '0')
+                if (X[1] == 'x')
+                    decimalX = Convert.ToInt32(X[2..], 16);
+                else
+                    decimalX = Convert.ToInt32(X[1..], 8);
+            else
+                decimalX = Convert.ToInt32(X, 10);
+
+            Console.WriteLine(decimalX);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using Algorithm.BOJ.BOJ_31925;
+﻿using Algorithm.BOJ.BOJ_11816;
 
 namespace Algorithm
 {
