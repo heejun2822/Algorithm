c7b369552cd2b7795a60cbab5b56b68d37ef7fb4
heejun2822 <godfam44@gmail.com>
2025-08-13 16:09:53 +0900

    new solution : BOJ_11942

---
+++ BOJ/BOJ_11942/Solution.cs
@@ -0,0 +1,15 @@
+namespace Algorithm.BOJ.BOJ_11942
+{
+    public class Solution
+    {
+        public static string[] InputPaths { get; private set; } =
+        [
+            "BOJ/BOJ_11942/input.txt",
+        ];
+
+        public static void Run(string[] args)
+        {
+            Console.WriteLine("고려대학교");
+        }
+    }
+}
