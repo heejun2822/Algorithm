56e039a488363246751bac665279e84e19bbcf35
heejun2822 <godfam44@gmail.com>
2025-08-21 14:54:39 +0900

    new solution : BOJ_14655

---
+++ BOJ/BOJ_14655/Solution.cs
@@ -0,0 +1,43 @@
+namespace Algorithm.BOJ.BOJ_14655
+{
+    public class Solution
+    {
+        public static string[] InputPaths { get; private set; } =
+        [
+            "BOJ/BOJ_14655/input.txt",
+        ];
+
+        public static void Run(string[] args)
+        {
+            StreamReader sr = Program.InputReader;
+            // StreamReader sr = new(new BufferedStream(Console.OpenStandardInput()));
+            StreamWriter sw = new(new BufferedStream(Console.OpenStandardOutput()));
+
+            int N = ReadInt(sr);
+
+            int score = 0;
+
+            for (int _ = 2 * N; _ > 0; _--)
+                score += Math.Abs(ReadInt(sr));
+
+            sw.WriteLine(score);
+
+            sr.Close();
+            sw.Close();
+        }
+
+        private static int ReadInt(StreamReader sr)
+        {
+            int c, val = 0, sign = 1;
+            while (true)
+            {
+                c = sr.Read();
+                if (c == ' ' || c == '\n' || c == -1) break;
+                if (c == '\r') { sr.Read(); break; }
+                if (c == '-') { sign = -1; continue; }
+                val = val * 10 + c - '0';
+            }
+            return val * sign;
+        }
+    }
+}
