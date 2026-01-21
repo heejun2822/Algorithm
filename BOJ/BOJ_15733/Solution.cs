be298002d9873bb57429976c6e437494bdea5aaa
heejun2822 <godfam44@gmail.com>
2026-01-21 13:31:41 +0900

    new solution : BOJ_15733

---
+++ BOJ/BOJ_15733/Solution.cs
@@ -0,0 +1,15 @@
+namespace Algorithm.BOJ.BOJ_15733
+{
+    public class Solution : SolutionBOJ<Solution>, ISolutionBOJ
+    {
+        public static string[] InputPaths { get; set; } =
+        [
+            "BOJ/BOJ_15733/input.txt",
+        ];
+
+        public static void Run(string[] args)
+        {
+            Console.WriteLine("I'm Sexy");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using Algorithm.BOJ.BOJ_15317;
+﻿using Algorithm.BOJ.BOJ_15733;
 
 namespace Algorithm
 {
