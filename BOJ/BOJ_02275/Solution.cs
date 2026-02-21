6cee619f20a4eba0459d08fc11ca736cf936520d
heejun2822 <godfam44@gmail.com>
2026-02-21 19:03:22 +0900

    new solution : BOJ_02275

---
+++ BOJ/BOJ_02275/Solution.cs
@@ -0,0 +1,76 @@
+namespace Algorithm.BOJ.BOJ_02275
+{
+    public class Solution : SolutionBOJ<Solution>, ISolutionBOJ
+    {
+        public static string[] InputPaths { get; set; } =
+        [
+            "BOJ/BOJ_02275/input.txt",
+        ];
+
+        public static void Run(string[] args)
+        {
+            StreamReader sr = Program.InputReader;
+            // StreamReader sr = new(new BufferedStream(Console.OpenStandardInput()));
+            StreamWriter sw = new(new BufferedStream(Console.OpenStandardOutput()));
+
+            int N = ReadInt(sr), H = ReadInt(sr);
+
+            int[] parents = new int[N + 1];
+            int[] weights = new int[N + 1];
+            int root = 0;
+
+            for (int i = 1; i <= N; i++)
+            {
+                parents[i] = ReadInt(sr);
+                weights[i] = ReadInt(sr);
+                if (parents[i] == 0)
+                    root = i;
+            }
+
+            bool[] isValid = new bool[N + 1];
+            int minCost = 0;
+
+            for (int i = 1; i <= N; i++)
+            {
+                if (isValid[i]) continue;
+
+                int height = 0;
+                ValidateNode(i, ref height);
+            }
+
+            sw.WriteLine(minCost);
+
+            sr.Close();
+            sw.Close();
+
+            void ValidateNode(int node, ref int height)
+            {
+                if (node == root) return;
+
+                height += weights[node];
+                ValidateNode(parents[node], ref height);
+
+                int cost = Math.Clamp(height - H, 0, weights[node]);
+                weights[node] -= cost;
+                minCost += cost;
+
+                isValid[node] = true;
+                height -= cost;
+            }
+        }
+
+        private static int ReadInt(StreamReader sr)
+        {
+            int c, val = 0, sign = 1;
+            while (true)
+            {
+                c = sr.Read();
+                if (c == ' ' || c == '\n' || c == -1) break;
+                if (c == '\r') { sr.Read(); break; }
+                if (c == '-') { sign = -1; continue; }
+                val = val * 10 + c - '0';
+            }
+            return val * sign;
+        }
+    }
+}
